d928e60b1094116797c24960bb6bdc74534b95e6
eojysele <eojysele@yandex.ru>
2023-09-13 21:11:31 +0300

    remove buffers swiching

--- nvim/lua/eojysele/keymaps/init.lua
+++ nvim/lua/eojysele/keymaps/init.lua
@@ -23,8 +23,6 @@ key_map("n", "<leader>sl", "<C-w>l")     -- to right split
 key_map("n", "<leader>sx", ":close<CR>") -- close current split window
 
 -- Buffers
-key_map("n", "<leader>bp", ":bprev<CR>")    -- go to prev buffer
-key_map("n", "<leader>bn", ":bnext<CR>")    -- go to next buffer
 key_map("n", "<leader>bx", ":bdelete<CR>")  -- close current buffer
 key_map("n", "<leader>bX", ":bdelete!<CR>") -- close current buffer
 
