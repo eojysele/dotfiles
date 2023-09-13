551b18ca175d84aa31111bb736c3d2337201a8aa
eojysele <eojysele@yandex.ru>
2023-09-13 21:34:31 +0300

    update keymaps

--- nvim/lua/eojysele/keymaps/init.lua
+++ nvim/lua/eojysele/keymaps/init.lua
@@ -49,23 +49,21 @@ key_map("n", "gd", vim.lsp.buf.definition)                                    --
 key_map("n", "gD", vim.lsp.buf.declaration)                                   -- go to declaration
 key_map("n", "gr", vim.lsp.buf.references)                                    -- list of references
 key_map("n", "gi", vim.lsp.buf.implementation)                                -- go to implementation
-key_map("n", "<leader>St", vim.lsp.buf.hover)                                 -- hover text
+key_map("n", "<leader>St", vim.lsp.buf.hover)                                 -- show hover text
 key_map("n", "<leader>Ss", vim.lsp.buf.signature_help)                        -- show signature
 key_map("n", "<leader>rn", vim.lsp.buf.rename)                                -- rename
 key_map("n", "<leader>ca", vim.lsp.buf.code_action)                           -- code actions
 key_map("n", '<leader>f', function() vim.lsp.buf.format { async = true } end) -- format file
 
 -- Debug
-key_map("n", "<leader>Bb", "<cmd>lua require'dap'.toggle_breakpoint()<CR>")                                           -- set breakpoint
-key_map("n", "<leader>Bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")        -- set condition breakpoint
-key_map("n", "<leader>Bl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>") -- set log point
-key_map("n", "<leader>Br", "<cmd>lua require'dap'.clear_breakpoints()<CR>")                                           -- clear breakpoints
-key_map("n", "<leader>Ba", "<cmd>Telescope dap list_breakpoints<CR>")                                                 -- list breakpoints
-key_map("n", "<leader>Dc", "<cmd>lua require'dap'.continue()<CR>")                                                    -- continue
-key_map("n", "<leader>Dj", "<cmd>lua require'dap'.step_over()<CR>")                                                   -- step over
-key_map("n", "<leader>Dk", "<cmd>lua require'dap'.step_into()<CR>")                                                   -- step into
-key_map("n", "<leader>Do", "<cmd>lua require'dap'.step_out()<CR>")                                                    -- step out
-key_map("n", "<leader>Dv", function() require "dap.ui.widgets".hover() end)                                           -- variables
+key_map("n", "<leader>Bb", "<cmd>lua require'dap'.toggle_breakpoint()<CR>") -- set breakpoint
+key_map("n", "<leader>Bx", "<cmd>lua require'dap'.clear_breakpoints()<CR>") -- clear breakpoints
+key_map("n", "<leader>Bl", "<cmd>Telescope dap list_breakpoints<CR>")       -- list breakpoints
+key_map("n", "<leader>Dc", "<cmd>lua require'dap'.continue()<CR>")          -- continue
+key_map("n", "<leader>Do", "<cmd>lua require'dap'.step_over()<CR>")         -- step over
+key_map("n", "<leader>Di", "<cmd>lua require'dap'.step_into()<CR>")         -- step into
+key_map("n", "<leader>DO", "<cmd>lua require'dap'.step_out()<CR>")          -- step out
+key_map("n", "<leader>Dv", function() require "dap.ui.widgets".hover() end) -- variables
 key_map("n", "<leader>Ds",
     function()
         local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes)
@@ -76,7 +74,6 @@ key_map("n", "<leader>F", "<cmd>Telescope live_grep<CR>")            -- grep
 key_map("n", "<leader>Ff", "<cmd>Telescope find_files<CR>")          -- find file
 key_map("n", "<leader>Fb", "<cmd>Telescope buffers<CR>")             -- find buffer
 key_map("n", "<leader>Fm", "<cmd>Telescope marks<CR>")               -- find mark
-key_map("n", "<leader>Fr", "<cmd>Telescope lsp_references<CR>")      -- find references
 key_map("n", "<leader>Fd", "<cmd>Telescope diagnostics bufnr=0<CR>") -- find diagnostics in buffer
 
 --------------------------------------
@@ -85,12 +82,9 @@ key_map("n", "<leader>Fd", "<cmd>Telescope diagnostics bufnr=0<CR>") -- find dia
 -- Java
 function P.java_key_map(bufnr)
     local jdtls = require("jdtls")
-    key_map("n", "<leader>ji", jdtls.organize_imports)                                      -- organize imports
-    key_map("n", "<leader>jv", jdtls.extract_variable)                                      -- extract variable
-    key_map("n", "<leader>jc", jdtls.extract_constant)                                      -- extract constant
-    key_map("v", "<leader>jm", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]]) -- extract method
-    key_map("n", "<leader>tc", jdtls.test_class)                                            -- test class
-    key_map("n", "<leader>tm", jdtls.test_nearest_method)                                   -- test method
+    key_map("n", "<leader>ji", jdtls.organize_imports)    -- organize imports
+    key_map("n", "<leader>tc", jdtls.test_class)          -- test class
+    key_map("n", "<leader>tm", jdtls.test_nearest_method) -- test method
 
     -- Spring Boot App
     local spring_app = require("eojysele.auxiliary-scripts.language.java.run-spring-app")
