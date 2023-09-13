33b0cea17068ea421d9658ff181260421e62ab5d
eojysele <eojysele@yandex.ru>
2023-09-13 21:06:25 +0300

    show full path in telescope

--- nvim/lua/eojysele/plugins/telescope/init.lua
+++ nvim/lua/eojysele/plugins/telescope/init.lua
@@ -1,11 +1,5 @@
 require('telescope').setup({
     defaults = {
-        path_display = {
-            shorten = {
-                len = 3, exclude = { 1, -1 }
-            },
-            truncate = true
-        },
         dynamic_preview_title = true,
     },
     extensions = {
