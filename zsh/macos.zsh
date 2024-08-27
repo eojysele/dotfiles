3667c4773a331db196664fb52122f074d14756cc
eojysele <eojysele@yandex.ru>
2024-08-27 19:12:10 +0300

    zsh: remove vi-mode plugin

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -22,5 +22,4 @@ export SDKMAN_DIR="$HOME/.sdkman"
 # Plugins
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 
