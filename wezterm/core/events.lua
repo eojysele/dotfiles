3b11e4fc644d4f172708987e7f209c46edd2e9f6
eojysele <eojysele@yandex.ru>
2024-11-02 16:50:22 +0300

    wezterm: update events

--- wezterm/core/events.lua
+++ wezterm/core/events.lua
@@ -4,13 +4,9 @@ function E.setup(wezterm)
 	wezterm.on("toggle-leader", function(window, _)
 		local overrides = window:get_config_overrides() or {}
 		if not overrides.leader then
-			-- replace it with an "impossible" leader that will enver be pressed
 			overrides.leader = { key = "_", mods = "CTRL|ALT|SUPER" }
-			wezterm.log_warn("[leader] clear")
 		else
-			-- restore to the main leader
 			overrides.leader = nil
-			wezterm.log_warn("[leader] set")
 		end
 		window:set_config_overrides(overrides)
 	end)
@@ -18,10 +14,8 @@ function E.setup(wezterm)
 	wezterm.on("toggle-tabbar", function(window, _)
 		local overrides = window:get_config_overrides() or {}
 		if overrides.enable_tab_bar == false then
-			wezterm.log_info("tab bar shown")
 			overrides.enable_tab_bar = true
 		else
-			wezterm.log_info("tab bar hidden")
 			overrides.enable_tab_bar = false
 		end
 		window:set_config_overrides(overrides)
