4f5c5e620c97460a69399335e0dc7793bb319aaf
eojysele <eojysele@yandex.ru>
2024-11-12 15:11:19 +0300

    nvim: update queries

--- nvim/queries/java/context.scm
+++ nvim/queries/java/context.scm
@@ -8,20 +8,3 @@
   body: (_) @context.end
 ) @context
 
-(if_statement
-  consequence: (_) @context.end
-) @context
-
-(for_statement
-  body: (_) @context.end
-) @context
-
-(enhanced_for_statement
-  body: (_) @context.end
-) @context
-
-(switch_expression) @context
-
-(switch_block_statement_group) @context
-
-(expression_statement) @context
