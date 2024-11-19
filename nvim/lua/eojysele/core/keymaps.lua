ce295835e2b7eea42647365d831f90077d9659e8
eojysele <eojysele@yandex.ru>
2024-11-19 11:18:56 +0300

    nvim: apply stylua

--- nvim/lua/eojysele/core/keymaps.lua
+++ nvim/lua/eojysele/core/keymaps.lua
@@ -11,8 +11,8 @@ function P.general_keymaps()
 	local builtin = require("telescope.builtin")
 
 	local keymaps = {
-		{ mode = "n", lhs = "<leader>=",  rhs = "<C-a>" },
-		{ mode = "n", lhs = "<leader>-",  rhs = "<C-x>" },
+		{ mode = "n", lhs = "<leader>=", rhs = "<C-a>" },
+		{ mode = "n", lhs = "<leader>-", rhs = "<C-x>" },
 		{ mode = "n", lhs = "<leader>sd", rhs = "<C-w>v" },
 		{ mode = "n", lhs = "<leader>sD", rhs = "<C-w>s" },
 		{ mode = "n", lhs = "<leader>s0", rhs = "<C-w>=" },
@@ -25,12 +25,12 @@ function P.general_keymaps()
 		{ mode = "n", lhs = "<leader>s_", rhs = "20<C-w>-" },
 		{ mode = "n", lhs = "<leader>s+", rhs = "20<C-w>+" },
 		{ mode = "n", lhs = "<leader>sx", rhs = ":close<CR>" },
-		{ mode = "n", lhs = "<leader>e",  rhs = "<cmd>NvimTreeToggle<CR>" },
+		{ mode = "n", lhs = "<leader>e", rhs = "<cmd>NvimTreeToggle<CR>" },
 		{ mode = "n", lhs = "<leader>fc", rhs = "<cmd>NvimTreeFindFile<CR>" },
-		{ mode = "n", lhs = "gd",         rhs = lsp_buf.definition },
-		{ mode = "n", lhs = "gD",         rhs = lsp_buf.declaration },
-		{ mode = "n", lhs = "gr",         rhs = lsp_buf.references },
-		{ mode = "n", lhs = "gi",         rhs = lsp_buf.implementation },
+		{ mode = "n", lhs = "gd", rhs = lsp_buf.definition },
+		{ mode = "n", lhs = "gD", rhs = lsp_buf.declaration },
+		{ mode = "n", lhs = "gr", rhs = lsp_buf.references },
+		{ mode = "n", lhs = "gi", rhs = lsp_buf.implementation },
 		{ mode = "n", lhs = "<leader>ch", rhs = lsp_buf.hover },
 		{ mode = "n", lhs = "<leader>cs", rhs = lsp_buf.signature_help },
 		{ mode = "n", lhs = "<leader>cd", rhs = vim.diagnostic.open_float },
@@ -63,9 +63,9 @@ end
 -- NvimTree
 function P.tree_keymaps(buffer)
 	local deleted_keymaps = {
-		{ mode = "n", lhs = "e",     buffer = buffer },
-		{ mode = "n", lhs = "f",     buffer = buffer },
-		{ mode = "n", lhs = "F",     buffer = buffer },
+		{ mode = "n", lhs = "e", buffer = buffer },
+		{ mode = "n", lhs = "f", buffer = buffer },
+		{ mode = "n", lhs = "F", buffer = buffer },
 		{ mode = "n", lhs = "<C-e>", buffer = buffer },
 	}
 
