fb4f69d51cfbd334c97e0d87dbc3234b9615bd77
eojysele <eojysele@yandex.ru>
2024-11-24 14:43:43 +0300

    nvim: update diagnostics config

--- nvim/lua/eojysele/core/ui/init.lua
+++ nvim/lua/eojysele/core/ui/init.lua
@@ -13,16 +13,40 @@ for type, icon in pairs(signs) do
 end
 
 vim.diagnostic.config({
+	severity_sort = true,
 	float = {
 		show_header = true,
 		source = "always",
 		suffix = "",
 	},
-	severity_sort = true,
-	virtual_text = false,
+	virtual_text = {
+		prefix = "",
+		spacing = 4,
+		severity = {
+			min = vim.diagnostic.severity.WARN,
+		},
+		format = function(diagnostic)
+			local max_length = 50
+			local symbols = {
+				[vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
+				[vim.diagnostic.severity.WARN] = icons.diagnostics.warn,
+				[vim.diagnostic.severity.INFO] = icons.diagnostics.info,
+				[vim.diagnostic.severity.HINT] = icons.diagnostics.hint,
+			}
+
+			local message = diagnostic.message
+			local severity = diagnostic.severity
+			if #message > max_length then
+				message = message:sub(1, max_length) .. "..."
+			end
+
+			return string.format("%s %s ", symbols[severity], message)
+		end,
+		signs = true,
+		update_in_insert = false,
+	},
 })
 
--- set all floating preview rounded border
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 	opts = opts or {}
