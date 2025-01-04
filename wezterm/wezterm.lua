e298ad529721d60fc3e75dfbeabc87285c1023ee
eojysele <eojysele@yandex.ru>
2025-01-04 19:18:07 +0300

    wezterm: disable window_close_confirmation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,6 +14,7 @@ config.initial_rows = 32
 config.scrollback_lines = 10000
 config.adjust_window_size_when_changing_font_size = false
 config.automatically_reload_config = true
+config.window_close_confirmation = 'NeverPrompt'
 
 local theme = require("core.theme")
 config.default_cursor_style = "SteadyBlock"
