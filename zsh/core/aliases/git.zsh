e0fbf29ff8a9a4f922131a8caf442e221938a96b
eojysele <eojysele@yandex.ru>
2025-01-09 12:06:32 +0300

    zsh: add git aliases

--- zsh/core/aliases/git.zsh
+++ zsh/core/aliases/git.zsh
@@ -1,5 +1,8 @@
 alias gs="git status"
 alias gc="git commit -m"
+alias gf="git fetch --prune origin"
+alias gF="git fetch --prune --all"
+alias gp="git pull origin"
 alias gP="git push -u origin"
 alias gR="git reset HEAD --hard"
 alias gC="git clean -i"
