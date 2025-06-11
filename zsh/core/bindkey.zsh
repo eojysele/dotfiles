0f29ca23ff0fd45cec99106c6ca7d6261b9e2843
eojysele <eojysele@yandex.ru>
2025-06-11 18:50:41 +0300

    update zsh config

---
+++ zsh/core/bindkey.zsh
@@ -0,0 +1,19 @@
+bindkey '^I' menu-complete
+bindkey '^[[Z' reverse-menu-complete
+bindkey '^[[1;2B' history-beginning-search-forward
+bindkey '^[[1;2A' history-beginning-search-backward
+
+bindkey '^[f' forward-word
+bindkey '^[b' backward-word
+
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
+
+bindkey '^[[1;6D' beginning-of-line
+bindkey '^[[1;6C' end-of-line
+
+bindkey '^U' backward-kill-line
+bindkey '^H' backward-kill-line
