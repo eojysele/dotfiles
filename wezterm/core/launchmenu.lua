b8c548f3adc3e0ab9be0dcd54b1ea647b13829a9
eojysele <eojysele@yandex.ru>
2025-06-11 18:49:58 +0300

    update wezterm config

--- wezterm/core/launchmenu.lua
+++ wezterm/core/launchmenu.lua
@@ -3,21 +3,47 @@ local launchmenu = {
 	launch_menu = nil,
 }
 
+local wezterm = require("wezterm")
 local platform = require("core.utils.platform")
+local table_utils = require("core.utils.table")
+
+local function get_wsl()
+	local wsl_menu = {}
+
+	local wsl_list = wezterm.default_wsl_domains()
+	for _, wsl in ipairs(wsl_list) do
+		local distribution = wsl.distribution
+		local wsl_command = {
+			label = "WSL: " .. distribution,
+			args = {
+				"wsl.exe",
+				"-d",
+				distribution,
+				"--cd",
+				"~",
+			},
+		}
+
+		table.insert(wsl_menu, wsl_command)
+	end
+
+	return wsl_menu
+end
 
 if platform.is_windows then
-	local wsl_default_args = { "wsl.exe", "--cd", "~" }
-	launchmenu.default_prog = wsl_default_args
-	launchmenu.launch_menu = {
+	local default_prog =  { "wsl.exe", "--cd", "~" }
+	local launch_menu = {
 		{
 			label = "PowerShell",
 			args = { "powershell.exe", "-NoLogo" },
 		},
-		{
-			label = "WSL (default)",
-			args = wsl_default_args,
-		},
 	}
+
+	table_utils.merge(launch_menu, get_wsl())
+
+	launchmenu.default_prog = default_prog
+	launchmenu.launch_menu = launch_menu
+
 end
 
 return launchmenu
