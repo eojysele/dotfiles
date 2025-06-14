a6b2ea41bfae51689d8b590de130368936c453d1
eojysele <eojysele@yandex.ru>
2025-06-14 21:30:29 +0300

    update wezterm bindings

--- wezterm/core/bindings/linux/keys.lua
+++ wezterm/core/bindings/linux/keys.lua
@@ -6,16 +6,13 @@ local wl_keys = require("core.bindings.shared.wl.keys").get()
 
 function K.get()
 	local keys = {
-		{ key = "LeftArrow", mods = "CTRL", action = actions.SendKey.Alt_B },
-		{ key = "LeftArrow", mods = "CTRL|SHIFT", action = actions.SendKey.Ctrl_A },
-		{ key = "RightArrow", mods = "CTRL", action = actions.SendKey.Alt_F },
-		{ key = "RightArrow", mods = "CTRL|SHIFT", action = actions.SendKey.Ctrl_E },
-		{ key = "Backspace", mods = "CTRL", action = actions.SendKey.Ctrl_W },
-		{ key = "Backspace", mods = "CTRL|SHIFT", action = actions.SendKey.Ctrl_U },
+		{ key = "LeftArrow", mods = "CTRL", action = actions.ShellKeys.BackwardWord },
+		{ key = "LeftArrow", mods = "CTRL|SHIFT", action = actions.ShellKeys.BeginningOfLine },
+		{ key = "RightArrow", mods = "CTRL", action = actions.ShellKeys.ForwardWord },
+		{ key = "RightArrow", mods = "CTRL|SHIFT", action = actions.ShellKeys.EndOfLine },
+		{ key = "Backspace", mods = "CTRL", action = actions.ShellKeys.BackwardKillWord },
+		{ key = "Backspace", mods = "CTRL|SHIFT", action = actions.ShellKeys.BackwardKillLine },
 		{ key = "V", mods = "CTRL|SHIFT", action = actions.SendKey.Ctrl_V },
-		{ key = "T", mods = "CTRL|SHIFT", action = actions.SendKey.Ctrl_T },
-		{ key = "W", mods = "CTRL|SHIFT", action = actions.SendKey.Ctrl_W },
-		{ key = "F", mods = "CTRL|SHIFT", action = actions.SendKey.Ctrl_F },
 	}
 	table_utils.merge(keys, wl_keys.get())
 
