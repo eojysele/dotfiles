13f3a71d3d50be899158e98e9a4cc58798e665cb
eojysele <eojysele@yandex.ru>
2025-06-14 17:22:09 +0300

    update wezterm bindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,21 +11,21 @@ config.initial_rows = 32
 config.scrollback_lines = 10000
 config.adjust_window_size_when_changing_font_size = false
 config.automatically_reload_config = true
-config.window_close_confirmation = "NeverPrompt"
 
 local theme = require("core.theme")
 config.default_cursor_style = "SteadyBlock"
 config.enable_scroll_bar = false
 config.use_fancy_tab_bar = true
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.colors = theme.get_colors()
 config.force_reverse_video_cursor = false
 config.window_frame = theme.get_window_frame(font.family, font.size)
 config.hide_mouse_cursor_when_typing = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
 config.show_close_tab_button_in_tabs = false
 config.show_new_tab_button_in_tab_bar = false
 config.native_macos_fullscreen_mode = true
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 config.window_padding = {
 	left = "0.5cell",
